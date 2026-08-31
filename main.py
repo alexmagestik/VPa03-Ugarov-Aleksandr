3d8134d94cc483e278adb39b193b572fef72f223
Alex Ugarov <alexuu@mail.ru>
2026-08-31 11:24:05 +0600

    parameter added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
+
 import requests
 
-r = requests.get("https://something.com")
+url = sys.argv[1] if len(sys.argv) > 1 else "https://something.com"
+
+r = requests.get(url)
 
 print(r.status_code)
 print(r.content)
